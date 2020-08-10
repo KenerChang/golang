bd8ae54ef084b6078b74c814b163b1e5cd871686
kenchang <workinkener@gmail.com>
2020-08-10 22:49:56 +0800

    test

--- rest/util/httpUtil.go
+++ rest/util/httpUtil.go
@@ -3,8 +3,48 @@ package util
 import (
 	"crypto/tls"
 	"encoding/json"
+	"fmt"
+	"github.com/KenerChang/golang/logger"
+	"github.com/KenerChang/golang/rest"
+	"github.com/gorilla/mux"
 	"net/http"
 )
+
+// SetRoutes recives routes info and return a mux router
+func SetRoutes(routes []rest.Route) *mux.Router {
+	apiRoutes := mux.NewRouter()
+	for _, route := range routes {
+		for _, endpoint := range route.Endpoints {
+			path := fmt.Sprintf("/api/v%d/%s", endpoint.Version, route.Name)
+			if endpoint.Path != "" {
+				path = path + "/" + endpoint.Path
+			}
+
+			logger.Info.Printf("set path: %s", path)
+
+			apiRoutes.
+				HandleFunc(path, func(w http.ResponseWriter, r *http.Request) {
+					defer func() {
+						if err := recover(); err != nil {
+							errMsg := fmt.Sprintf("%#v", err)
+							WriteWithStatus(w, errMsg, http.StatusInternalServerError)
+							logger.Error.Println(r, "Panic error:", errMsg)
+						}
+					}()
+
+					endpoint.Callback(w, r)
+				}).
+				Methods(endpoint.Method)
+		}
+
+		// init module
+		if route.InitFunc != nil {
+			route.InitFunc()
+		}
+	}
+
+	return apiRoutes
+}
 
 // WriteJSON is a sugar function which handle response json
 func WriteJSON(w http.ResponseWriter, obj interface{}) error {
